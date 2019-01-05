8ba6278bf8b9125ac1901abb80f269be215b0152
eunwoo <aufcl4858@naver.com>
2019-01-05 14:31:48 +0900

    #2.25 More Controlers

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,11 +22,14 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-  res.render('logout', { pageTitle: 'Log Out' });
 export const userDetail = (req, res) =>
   res.render('userDetail', { pageTitle: 'User Detail' });
 export const editProfile = (req, res) =>
   res.render('editProfile', { pageTitle: 'Edit Profile' });
 export const changePassword = (req, res) =>
   res.render('changePassword', { pageTitle: 'Change Password' });
+
+export const logout = (req, res) => {
+  // TODO: Process Log out
+  res.redirect(routes.home);
+};
